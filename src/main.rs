88e4b411b4dfbd88bca870ef338754e0b5b73b1d
Konbor704 <konbor12@gmail.com>
2024-05-15 19:42:51 +0200

    reset

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,23 @@ use bevy::{prelude::*, window::WindowResolution};
 
 mod ui;
 
-use self::ui::UiCustomPlugin;
+// use self::ui::UiCustomPlugin;
+#[derive(Debug, Eq, Clone, PartialEq, Hash, Default, States)]
+pub enum AppState {
+    #[default]
+    MainMenu,
+    InGame,
+    Paused,
+    End,
+}
+
+#[derive(Component)]
+pub struct MainCamera;
 
 fn main() {
     let mut app = App::new();
+
+    app.init_state::<AppState>();
 
     app.add_plugins(DefaultPlugins.set(WindowPlugin {
         primary_window: Some(Window {
@@ -16,7 +29,7 @@ fn main() {
         ..Default::default()
     }));
 
-    app.add_plugins(UiCustomPlugin);
+    // app.add_plugins(UiCustomPlugin);
 
     app.add_systems(Startup, setup);
 
@@ -25,5 +38,5 @@ fn main() {
 }
 
 fn setup(mut c: Commands) {
-    c.spawn(Camera2dBundle::default());
+    c.spawn((Camera2dBundle::default(), MainCamera));
 }
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,13 +1,13 @@
 use bevy::prelude::*;
-pub mod bottombar;
+// pub mod bottombar;
 // pub mod topbar;
 
-use self::{bottombar::UiBottomBarPlugin /* topbar::UiTopBarPlugin */};
+// use self::{bottombar::UiBottomBarPlugin /* topbar::UiTopBarPlugin */};
 
-pub struct UiCustomPlugin;
+// pub struct UiCustomPlugin;
 
-impl Plugin for UiCustomPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_plugins((UiBottomBarPlugin /* UiTopBarPlugin */,));
-    }
-}
+// impl Plugin for UiCustomPlugin {
+//     fn build(&self, app: &mut App) {
+//         app.add_plugins((UiBottomBarPlugin /* UiTopBarPlugin */,));
+//     }
+// }
